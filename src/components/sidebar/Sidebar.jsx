ca2b2ad89f8ca4fc28c82c48bdddda5b19d18e53
gabrielCarrenio <carrenocarlosgabriel@gmail.com>
2022-12-19 19:59:41 -0300

    add sidebar

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -2,14 +2,22 @@ import BorderAllOutlinedIcon from '@mui/icons-material/BorderAllOutlined';
 import GradeOutlinedIcon from '@mui/icons-material/GradeOutlined';
 import FavoriteBorderOutlinedIcon from '@mui/icons-material/FavoriteBorderOutlined';
 import './sidebar.scss';
+import { MoviesContext } from '../../context/MoviesContext';
+import { useContext } from 'react';
 
 export const Sidebar = () => {
+  const { setSearchTerm } = useContext(MoviesContext);
+
+  const handleSetDiscover = () => {
+    setSearchTerm('');
+  };
+
   return (
     <div className="sidebar">
-      <div>
+      <button onClick={handleSetDiscover}>
         <BorderAllOutlinedIcon />
-        <span>Beginning</span>
-      </div>
+        <span>Discover</span>
+      </button>
       <div>
         <GradeOutlinedIcon />
         <span>Favorite</span>
