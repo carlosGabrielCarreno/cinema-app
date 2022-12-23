0959036a935ac7e9b8d98282618a7935998372a1
gabrielCarrenio <carrenocarlosgabriel@gmail.com>
2022-12-23 13:41:38 -0300

    add footer component for detailMovie

--- src/components/detailMovie/DetailMovie.jsx
+++ src/components/detailMovie/DetailMovie.jsx
@@ -4,6 +4,7 @@ import { MoviesContext } from '../../context/MoviesContext';
 import { Spinner } from '../spinner/Spinner';
 import HomeIcon from '@mui/icons-material/Home';
 import './_detailMovie.scss';
+import { Footer } from '../footer/Footer';
 
 export const DetailMovie = () => {
   const { dataMovieDetail, setIdMovieDetail, isLoading } =
@@ -31,36 +32,41 @@ export const DetailMovie = () => {
               <HomeIcon fontSize="large" />
             </Link>
             <div className="container-data">
-              <img
-                className="img-detail-movie"
-                src={
-                  'https://image.tmdb.org/t/p/w300' +
-                  dataMovieDetail?.backdrop_path
-                }
-                alt={dataMovieDetail?.original_title}
-              />
-              <h2>{dataMovieDetail?.original_title}</h2>
-              <p className="overview">
-                {' '}
-                <span>overview: </span> {dataMovieDetail?.overview}
-              </p>
-              <ul className="info-list">
-                <li>
-                  <span>vote average:</span> {dataMovieDetail?.vote_average}
-                </li>
-                <li>
-                  <span>release date:</span> {dataMovieDetail?.release_date}
-                </li>
-                <li>
-                  <span>popularity:</span> {dataMovieDetail?.popularity}
-                </li>
-              </ul>
-              <ul className="genres">
-                {dataMovieDetail?.genres.map(({ id, name }) => (
-                  <li key={id}>{name}</li>
-                ))}
-              </ul>
+              <div className="container-title-and-img">
+                <img
+                  className="img-detail-movie"
+                  src={
+                    'https://image.tmdb.org/t/p/w300' +
+                    dataMovieDetail?.backdrop_path
+                  }
+                  alt={dataMovieDetail?.original_title}
+                />
+                <h2>{dataMovieDetail?.original_title}</h2>
+              </div>
+              <div className="container-info-detail-movie">
+                <p className="overview">
+                  {' '}
+                  <span>overview: </span> {dataMovieDetail?.overview}
+                </p>
+                <ul className="info-list">
+                  <li>
+                    <span>vote average:</span> {dataMovieDetail?.vote_average}
+                  </li>
+                  <li>
+                    <span>release date:</span> {dataMovieDetail?.release_date}
+                  </li>
+                  <li>
+                    <span>popularity:</span> {dataMovieDetail?.popularity}
+                  </li>
+                </ul>
+                <ul className="genres">
+                  {dataMovieDetail?.genres.map(({ id, name }) => (
+                    <li key={id}>{name}</li>
+                  ))}
+                </ul>
+              </div>
             </div>
+            <Footer />
           </div>
         </>
       )}
