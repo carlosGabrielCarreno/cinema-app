b7c24fc987be3575a6d579a758d75e73de29d00c
gabrielCarrenio <carrenocarlosgabriel@gmail.com>
2023-01-10 11:10:32 -0300

    work carousel styles

--- src/components/carousel/Carousel.jsx
+++ src/components/carousel/Carousel.jsx
@@ -1,16 +1,16 @@
 import { useState } from 'react';
-import ArrowLeftIcon from '@mui/icons-material/ArrowLeft';
-import ArrowRightIcon from '@mui/icons-material/ArrowRight';
+import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 import './_carousel.scss';
 
 export const Carousel = () => {
   const images = [
-    'movie1.jpg',
-    'movie2.jpg',
+    'movie1.png',
+    'movie2.png',
     'movie3.jpg',
     'movie4.jpg',
-    'movie5.jpg',
-    'movie6.png',
+    'movie6.jpg',
+    'movie7.jpg',
   ];
   const [selectedIndex, setSelectedIndex] = useState(0);
   const [selectedImage, setSelectedImage] = useState(images[0]);
@@ -43,11 +43,11 @@ export const Carousel = () => {
   };
   return (
     <div className="container-carousel">
-      <button onClick={previous}>
-        <ArrowLeftIcon fontSize="large" />
+      <button className="arrow-carousel left" onClick={previous}>
+        <ArrowBackIosNewIcon fontSize="large" />
       </button>
-      <button onClick={nextPage}>
-        <ArrowRightIcon fontSize="large" />
+      <button className="arrow-carousel right" onClick={nextPage}>
+        <ArrowForwardIosIcon fontSize="large" />
       </button>
       <img
         className={`image-carousel ${loaded ? 'loaded' : ''}`}
